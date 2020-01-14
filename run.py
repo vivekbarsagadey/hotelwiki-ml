50c2f2e0fbe039a6262986e5780ba9e3447a0d94
amoljagadambe <amoljagadambe0076@gmail.com>
2020-01-14 15:36:23 +0530

    Added the File Checker for dotenv  File

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from application import app
+from dotenv import load_dotenv
+import os
 
+env_path = '{0}\\.env'.format(os.getcwd())
+load_dotenv(dotenv_path=env_path)
 
 if __name__ == '__main__':
     app.run()
