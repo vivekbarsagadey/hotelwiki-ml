03a84b7e0ee42f86a200b26cd44f86196d03ce0a
amoljagadambe <amoljagadambe0076@gmail.com>
2020-01-15 12:56:34 +0530

    Added ProxyfFix for HTTP and HTTPS Rquests Handling

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restplus import Api
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 CORS(app)
 
 app.config.from_envvar('APP_SETTINGS')
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 api = Api(app, title='Text Generation', description='Created by Whiz IT Services', default='Flask',
           default_label='Controllers', doc='/swagger-ui',
